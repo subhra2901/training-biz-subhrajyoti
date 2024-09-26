aa2050e350ec1414d42ffb4b4af2a826e9c799ed
subhra2901 <subhrajyotin195@gmail.com>
2024-09-26 10:50:01 +0000

    2024-09-26 10:50:01

--- git_cron_job.sh
+++ git_cron_job.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-cd ~/training-biz-subhrajyoti
+cd ~/cloneassignment/training-biz-subhrajyoti
 cat ~/.bash_history  > /home/ec2-user/cloneassignment/training-biz-subhrajyoti/commandslist.txt
+sync
 git add .
 git commit -m "$(date '+%Y-%m-%d %H:%M:%S')"
 git push origin main
