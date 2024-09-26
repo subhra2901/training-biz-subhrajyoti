27dbeb63090a93b5850ce7e75b670ad675f26d17
subhra2901 <subhrajyotin195@gmail.com>
2024-09-26 12:28:10 +0000

    2024-09-26 12:28:10

--- git_cron_job.sh
+++ git_cron_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export HISTTIMEFORMAT="%F %T "
 source ~/.bashrc
-history > ~/cloneassignment/training-biz-subhrajyoti/commandslist.txt
+history -a > ~/cloneassignment/training-biz-subhrajyoti/commandslist.txt
 cd ~/cloneassignment/training-biz-subhrajyoti
 git add .
 git commit -m "$(date '+%Y-%m-%d %H:%M:%S')"
