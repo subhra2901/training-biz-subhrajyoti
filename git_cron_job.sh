db2168cb7a78bf5c5722ad2a1512f6c280f99f50
subhra2901 <subhrajyotin195@gmail.com>
2024-09-26 11:04:28 +0000

    2024-09-26 11:04:28

--- git_cron_job.sh
+++ git_cron_job.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+export HISTTIMEFORMAT="%F %T "
+source ~/.bashrc
+history > ~/cloneassignment/training-biz-subhrajyoti/commandslist.txt
 cd ~/cloneassignment/training-biz-subhrajyoti
-cat /home/ec2-user/commandslist.txt  > /home/ec2-user/cloneassignment/training-biz-subhrajyoti/commandslist.txt
-sync
 git add .
 git commit -m "$(date '+%Y-%m-%d %H:%M:%S')"
 git push origin main
